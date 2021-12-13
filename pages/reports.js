23be8aa1fc9037fa411a6a8bc0b9aa21e49d7b91
Ranjith Kumar <ranjithkumar.a@listertechnologies.com>
2021-12-13 18:51:15 +0530

    changing domains

--- pages/reports.js
+++ pages/reports.js
@@ -77,7 +77,7 @@ export default function Reports({transactions, error}) {
 }
 
 async function GetTransactionDetails(body){
-    const res = await fetch('https://money-transfer-phi.vercel.app'+'/api/transaction',{
+    const res = await fetch('https://money-transfer-2021.vercel.app/'+'/api/transaction',{
         method: 'POST',
         body: JSON.stringify(body),
         headers:{
